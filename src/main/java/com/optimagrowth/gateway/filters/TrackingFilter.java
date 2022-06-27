f3c10c757183262cb1b649913984731217572afe
Dang Giang <gianggiang1512@gmail.com>
2022-06-28 00:33:01 +0700

    get username from requestHeaders

--- src/main/java/com/optimagrowth/gateway/filters/TrackingFilter.java
+++ src/main/java/com/optimagrowth/gateway/filters/TrackingFilter.java
@@ -1,5 +1,7 @@
 package com.optimagrowth.gateway.filters;
 
+import org.apache.commons.codec.binary.Base64;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +39,7 @@ public class TrackingFilter implements GlobalFilter {
                     "tmx-correlation-id generated in tracking filter: {}.", correlationId
             );
         }
+        logger.debug("The authentication name from the token is: {}", getUsername(requestHeaders));
         return chain.filter(exchange);
     }
 
@@ -50,5 +53,28 @@ public class TrackingFilter implements GlobalFilter {
         } else {
             return false;
         }
+    }
+
+    private String getUsername(HttpHeaders requestHeaders){
+        String username = "";
+        if(filterUtils.getAuthToken(requestHeaders) != null){
+            String authToken = filterUtils.getAuthToken(requestHeaders).replace("Bearer ", "");
+            JSONObject jsonObj = decodeJWT(authToken);
+            try {
+                username = jsonObj.getString("preferred_username");
+            } catch (Exception e){
+                logger.debug(e.getMessage());
+            }
+        }
+        return username;
+    }
+
+    private JSONObject decodeJWT(String JWTToken) {
+        String[] split_string = JWTToken.split("\\.");
+        String base64EncodedBody = split_string[1];
+        Base64 base64Url = new Base64(true);
+        String body = new String(base64Url.decode(base64EncodedBody));
+        JSONObject jsonObj = new JSONObject(body);
+        return jsonObj;
     }
 }
