c063033085af30820ff4ee8306221c1d45b5ec9f
Dang Giang <gianggiang1512@gmail.com>
2022-07-18 23:11:52 +0700

    Add traceId in postFilter response header

--- src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
+++ src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
@@ -1,12 +1,16 @@
 package com.optimagrowth.gateway.filters;
 
+import brave.Span;
+import brave.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -15,19 +19,27 @@ public class ResponseFilter {
     private static final Logger logger = LoggerFactory.getLogger(ResponseFilter.class);
 
     @Autowired
+    private Tracer tracer;
+
+    @Autowired
     private FilterUtils filterUtils;
 
     @Bean
     public GlobalFilter postGlobalFilter(){
         return (exchange, chain) -> {
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-                String correlationId = filterUtils.getCorrelationId(requestHeaders);
-                logger.debug("Adding the correlation id to the outbound headers. {}", correlationId);
+            return chain.filter(exchange).
+                    then(Mono.fromRunnable(() -> {
+                        //HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+                        //String correlationId = filterUtils.getCorrelationId(requestHeaders);
+                        Span span = tracer.currentSpan();
+                        if(span != null) {
+                            String traceId = span.context().traceIdString();
+                            logger.debug("Adding the correlation id to the outbound headers. {}", traceId);
+                            exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, traceId);
+                        }
 
-                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
-                logger.debug("Completing outgoing request for {}", exchange.getRequest().getURI());
-            }));
+                        logger.debug("Completing outgoing request for {}", exchange.getRequest().getURI());
+                    }));
         };
     }
 }
